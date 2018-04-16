1b8ba1d3019f7cdb9f9f3830adbc848ce0e3bda3
Hitoshi Saiwaki <hitoshi.ee@gmail.com>
2018-04-16 21:06:21 +0900

    ...

--- source/About/Index.js
+++ source/About/Index.js
@@ -1,5 +1,18 @@
 import React, { Component } from 'react'
 import { Helmet } from "react-helmet"
+import { withGoogleMap, withScriptjs, GoogleMap, Marker } from "react-google-maps"
+
+const MapWithAMarker = withScriptjs(withGoogleMap(props =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    mapTypeId="roadmap"
+  >
+    <Marker
+      position={{ lat: -34.397, lng: 150.644 }}
+    />
+  </GoogleMap>
+));
 
 export default class Index extends Component {
 
@@ -21,6 +34,12 @@ export default class Index extends Component {
         <div>
           about
         </div>
+        <MapWithAMarker
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyB8H5B22SGUDi4CiPkGd4c__UDAHH4bt4I"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `300px` }} />}
+          mapElement={<div style={{ height: `300px` }} />}
+        />
       </div>
     )
   }
